c880e9d5f0d2089a45707f382852f7a6f235302c
RyanStLouis <ryan.stlouis@ocsbstudent.ca>
2015-10-08 13:05:16 -0400

    This a real walking man. Yup.

---
+++ WalkingMan/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WalkingMan
+{
+    partial class frmWalkingMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picWalkingMan = new System.Windows.Forms.PictureBox();
+            this.btnWalk = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picWalkingMan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picWalkingMan
+            // 
+            this.picWalkingMan.Location = new System.Drawing.Point(54, 39);
+            this.picWalkingMan.Name = "picWalkingMan";
+            this.picWalkingMan.Size = new System.Drawing.Size(147, 163);
+            this.picWalkingMan.TabIndex = 0;
+            this.picWalkingMan.TabStop = false;
+            // 
+            // btnWalk
+            // 
+            this.btnWalk.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWalk.Location = new System.Drawing.Point(99, 208);
+            this.btnWalk.Name = "btnWalk";
+            this.btnWalk.Size = new System.Drawing.Size(75, 23);
+            this.btnWalk.TabIndex = 1;
+            this.btnWalk.Text = "Walk";
+            this.btnWalk.UseVisualStyleBackColor = true;
+            this.btnWalk.Click += new System.EventHandler(this.btnWalk_Click);
+            // 
+            // frmWalkingMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnWalk);
+            this.Controls.Add(this.picWalkingMan);
+            this.Name = "frmWalkingMan";
+            this.Text = "Walking Man";
+            ((System.ComponentModel.ISupportInitialize)(this.picWalkingMan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picWalkingMan;
+        private System.Windows.Forms.Button btnWalk;
+    }
+}
+
---
+++ WalkingMan/Form1.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * Created by: Alex Mathias
+ * Created on: 08-sept-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 3-07
+ * This program displays a man walking.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace WalkingMan
+{
+    public partial class frmWalkingMan : Form
+    {
+        public frmWalkingMan()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWalk_Click(object sender, EventArgs e)
+        {
+            int loopCounter = 0;
+            do
+            {
+                if (loopCounter == 0)
+                {
+
+                    this.picWalkingMan.Image = WalkingMan.Properties.Resources.walk2;
+                }
+                else if (loopCounter == 1)
+                {
+                    this.picWalkingMan.Image = WalkingMan.Properties.Resources.walk3;
+                }
+                else if (loopCounter == 2)
+                {
+                    this.picWalkingMan.Image = WalkingMan.Properties.Resources.walk4;
+                }
+                else if (loopCounter == 3)
+                {
+                    this.picWalkingMan.Image = WalkingMan.Properties.Resources.walk5;
+                }
+                else if (loopCounter == 4)
+                {
+                    this.picWalkingMan.Image = WalkingMan.Properties.Resources.walk6;
+                }
+                else if (loopCounter == 5)
+                {
+                    this.picWalkingMan.Image = WalkingMan.Properties.Resources.walk7;
+                }
+                else if (loopCounter == 6)
+                {
+                    this.picWalkingMan.Image = WalkingMan.Properties.Resources.walk8;
+                }
+                else if (loopCounter == 7)
+                {
+                    this.picWalkingMan.Image = WalkingMan.Properties.Resources.walk9;
+                }
+                else if (loopCounter == 8)
+                {
+                    this.picWalkingMan.Image = WalkingMan.Properties.Resources.walk10;
+                }
+                else if (loopCounter == 9)
+                {
+                    this.picWalkingMan.Image = WalkingMan.Properties.Resources.walk1;
+                }
+                loopCounter = loopCounter + 1;
+
+                //pause
+                this.Refresh();
+                Thread.Sleep(100);
+
+            } while (loopCounter < 10);
+        }
+    }
+}
